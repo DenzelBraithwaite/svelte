67d495b644c3ba0b8c7e94e1e37ec8965f76f9bc
Denzel Braithwaite <braithwaite.de@gmail.com>
2023-01-17 12:28:07 -0500

    changed file structure

---
+++ project-setup/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let name;
+	let age = 27;
+
+	function incrementAge() {
+		age++;
+		if (age > 30) {
+			name = 'oldy😥';
+		};
+	};
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>👋🏽Hello, my name is {name}, I'm {age} years old!</h1>
+<p>Unless you press on that button down there...</p>
+<button on:click={incrementAge}>Don't click me</button>
