ed30f27c423f6247e889849db64d156cf24e56bc
Denzel Braithwaite <85902450+DenzelBraithwaite@users.noreply.github.com>
2023-01-22 13:52:09 -0500

    basic init svelte tutorialk

--- project-setup/src/App.svelte
+++ project-setup/src/App.svelte
@@ -1,13 +1,5 @@
 <script>
 	export let name;
-	let age = 27;
-
-	function incrementAge() {
-		age++;
-		if (age > 30) {
-			name = 'oldy😥';
-		};
-	};
 </script>
 
 <style>
@@ -16,6 +8,4 @@
 	}
 </style>
 
-<h1>👋🏽Hello, my name is {name}, I'm {age} years old!</h1>
-<p>Unless you press on that button down there...</p>
-<button on:click={incrementAge}>Don't click me</button>
+<h1>Hello {name}!</h1>
