3be256ba8a65ffb05e69a75a213c8b13433807ed
Denzel Braithwaite <85902450+DenzelBraithwaite@users.noreply.github.com>
2023-01-28 13:33:29 -0500

    conditionally rendered contact card

--- section3_project1/src/App.svelte
+++ section3_project1/src/App.svelte
@@ -1,10 +1,15 @@
 <script>
   import ContactCard from "./ContactCard.svelte";
 
-  let name = "Max";
+  let name = "";
   let title = "";
   let image = "";
   let description = "";
+  let submitted = false;
+
+  function addCard() {
+    submitted = true;
+  }
 </script>
 
 <style>
@@ -17,20 +22,27 @@
 <div id="form">
   <div class="form-control">
     <label for="userName">User Name</label>
-    <input type="text" bind:value={name} id="userName" />
+    <input placeholder="Ex: John" type="text" bind:value={name} id="userName" />
   </div>
   <div class="form-control">
     <label for="jobTitle">Job Title</label>
-    <input type="text" bind:value={title} id="jobTitle" />
+    <input placeholder="Ex: Smith" type="text" bind:value={title} id="jobTitle" />
   </div>
   <div class="form-control">
     <label for="image">Image URL</label>
-    <input type="text" bind:value={image} id="image" />
+    <input placeholder="https://media.istockphoto.com/id/642809804"
+    type="text"
+    bind:value={image}
+    id="image" />
   </div>
   <div class="form-control">
     <label for="desc">Description</label>
-    <textarea rows="3" bind:value={description} id="desc" />
+    <textarea placeholder="I love chess, coding and coffee" rows="3" bind:value={description} id="desc" />
   </div>
 </div>
 
-<ContactCard userName={name} jobTitle={title} {description} userImage={image} />
+{#if submitted}
+  <ContactCard userName={name} jobTitle={title} {description} userImage={image} />
+{/if}
+
+<button on:click={addCard}>Create Card</button>
