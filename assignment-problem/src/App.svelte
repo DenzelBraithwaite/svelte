9ed65bc2b0abfab710647af701dc5ffd5d2d4005
Denzel Braithwaite <85902450+DenzelBraithwaite@users.noreply.github.com>
2023-01-28 13:07:32 -0500

    finished first ass.

---
+++ assignment-problem/src/App.svelte
@@ -0,0 +1,53 @@
+<script>
+  import Output from "./lib/Output.svelte";
+  let goal = '';
+</script>
+
+<style>
+.form-group {
+  border: 1px solid #444;
+  border-radius: 8px;
+  display: inline-block;
+  padding: 1rem;
+  box-shadow: 4px 0 10px #4444443f;
+  background-color: rgb(219, 228, 255);
+}
+
+.form-group h3 {
+  font-weight: 700;
+}
+
+.form-group input {
+  border: 1px solid #444;
+  border-radius: 0.25rem;
+}
+</style>
+
+<h1>Assignment</h1>
+
+<p>Solve these tasks.</p>
+
+<ol>
+  <li>Add an input field that allows users to enter a course goal.</li>
+  <li>Output the user input in a h1 tag.</li>
+  <li>
+    Color the output red (e.g. by adding a class) if it contains at least one exclamation mark.
+  </li>
+  <li>
+    Put the h1 tag + output into a separate component to which you pass the user input
+  </li>
+</ol>
+<br>
+
+<div class="form-group">
+  <h3>Enter a course goal</h3>
+  <input
+    type="text"
+    placeholder="Get 90's in French"
+    bind:value={goal}>
+</div>
+
+<Output {goal}/>
+<Output {goal}/>
+<Output {goal}/>
+<Output {goal}/>
---
+++ assignment-problem/src/lib/Output.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let goal;
+
+</script>
+
+<style>
+.red {
+  color: red;
+} 
+
+</style>
+<h1 class:red="{goal.includes('!')}">Your goal: {goal}</h1>
