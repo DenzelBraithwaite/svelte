fdc48c157b75b7433f930cba9f5fe02a2f5c3955
Denzel Braithwaite <85902450+DenzelBraithwaite@users.noreply.github.com>
2023-03-08 15:10:54 -0500

    practiced the fetch API a few times

--- api_testing/src/App.svelte
+++ api_testing/src/App.svelte
@@ -1,84 +1,82 @@
 <script lang="ts">
-  let fName;
-  let lName;
-  let age;
-  let isLoading = false;
-  let teachers = [];
+  import { onMount } from 'svelte';
 
-  function addTeacher() {
-    isLoading = true;
-    const teacher =   {
-    firstName: fName,
-    lastName: lName,
-    age: age
-  };
+  let loading = false;
+  let first = '';
+  let last = '';
+  let students = [];
 
-    teachers = [...teachers, teacher]
+  onMount(() => {
+    loading = true;
+    fetch('https://svelte-api-testing-default-rtdb.firebaseio.com/students.json')
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Somethign went very wrong :c')
+      }
 
-  fetch('https://svelte-api-testing-default-rtdb.firebaseio.com/tests.json', {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(teacher),
+      return res.json()
+    }).then(data => {
+      const values = Object.values(data);
+      values.forEach(student => {
+        students = [student, ...students]
+      })
+      loading = false;
+    })
+    .catch(err => {
+      console.log(err);
+      loading = false;
+    })
   })
-  .then(response => {
-    isLoading = false;
-    if (!response.ok) {
-      throw new Error('Failed');
-    }
-  }).catch(error => {
-    isLoading = false;
-    console.log(error);
-  })
+
+  function addStudent() {
+  loading = true;
+  const student = {
+    firstName: first,
+    lastName: last
   }
 
-  // Load database data
-  fetch('https://svelte-api-testing-default-rtdb.firebaseio.com/tests.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Failed!');
-    }
-    return response.json()
+  students = [...students, student];
+
+  fetch('https://svelte-api-testing-default-rtdb.firebaseio.com/students.json', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(student)
   })
-  .then(data => {
-    let values = Object.values(data);
-    // let keys = Object.keys(data);
-    console.log('Values:');
-    console.log(values);
-    console.log('');
-    values.forEach((teacher) => {
-      teachers = [...teachers, teacher]
-    })
-    })
-  .catch(error => {
-    console.log(error)})
+  .then(res => {
+    if (!res.ok) {
+      throw new Error('Something went wrong :(')
+    }
+    loading = false;
+  })
+  .catch(err => {
+    loading = false;
+    console.log(err);
+  })
+}
 </script>
 
 <main>
-  
-  <h1>Teacher Form</h1>
+  <h1>Student Form</h1>
   <div class="form-control">
-    <label for="">First Name</label>
-    <input type="text" bind:value={fName}>
+    <label for="first-name">First Name</label>
+    <input bind:value={first} id="first-name" name="last-name" type="text">
   </div>
   <div class="form-control">
-    <label for="">Last Name</label>
-    <input type="text" bind:value={lName}>
+    <label for="last-name">Last Name</label>
+    <input bind:value={last} id="last-name" name="last-name" type="text">
   </div>
-  <div class="form-control">
-    <label for="">Age</label>
-    <input type="text" bind:value={age}>
-  </div>
-  <button on:click={addTeacher}>Submit</button>
-  
-  {#if isLoading}
-    <p class="loading">Loading . . .</p>
+  <button on:click={addStudent}>Submit</button>
+
+  {#if loading}
+    <p class="loading">Loading ...</p>
   {:else}
-    {#each teachers as teacher}
-    <div class="teacher-card">
-      <p>First Name: {teacher.firstName}</p>
-      <p>Last Name: {teacher.lastName}</p>
-      <p>Age: {teacher.age}</p>
-    </div>
+    {#each students as student}
+      <div class="student-card">
+        <p>First name: {student.firstName}</p>
+        <p>Last name: {student.lastName}</p>
+      </div>
     {/each}
   {/if}
 </main>
@@ -152,7 +150,7 @@
     background-color: #49bb58;
   }
 
-  .teacher-card {
+  .student-card {
     background-color: #edfff0;
     box-shadow: 0 2px 8px #49bb5836;
     border-radius: 5px;
